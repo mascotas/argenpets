0f5c9a78655dbaa13b28fda2d5336865c4ea1afe
Alejandro M <alejandro@Ales-MacBook-Air.local>
2013-11-20 21:26:30 -0300

    Uncommited

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,41 +1,50 @@
-
-
-
 module.exports = function(app){
 
-Get = function(req, res) {
-  console.log("GET");
-};
+	Get = function(req, res) {
+		console.log(req.body);
+		console.log('GET');
 
-Delete = function(req, res) {
-  console.log("DELETE");
-};
+		var usuario = require('../models/usuario');
+		/*
+		usuario.FindOne(  { id : req.body.id }, function(err, result, fields){
+			if(!err){
+				console.log(result);
+			}else{
+				console.log(err);
+			}
+		});
+		*/
+	};
 
-Post = function(req, res) {
-  console.log("POST");
-};
+	Delete = function(req, res) {
+		console.log("DELETE");
+	};
 
-Put = function(req, res) {
-  console.log("PUT");
-};
+	Post = function(req, res) {
+		var usuario = require('../models/usuario');
 
+		console.log(req.body);
+		console.log("POST");
+	};
 
-
-controller = function(app){
-
- app.get('/usuario/*', this.Get );
-
- app.post('/usuario/*', this.Post);    
-
- app.delete('/usuario/*', this.Delete);    
-
- app.put('/usuario/*', this.Put);    
-
-};
-
-controller(app);
+	Put = function(req, res) {
+		console.log("PUT");
+	};
 
 
+	controller = function(app){
+
+		app.get('/usuario?*', this.Get );
+
+		app.post('/usuario', this.Post);
+
+		app.delete('/usuario/*', this.Delete);
+
+		app.put('/usuario/*', this.Put);
+
+	};
+
+	controller(app);
 }
 
 
